20540a7e3d88828aa29dbb67424a7653358d911c
AryansVj <aryans.viraj@gmail.com>
2024-05-24 14:41:04 +0530

    Developed as a single page application to dynamically load content when clicked on nav-bar links

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,67 @@
+function observeSections(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+function loadSections() {
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver(observeSections, options);
+    const sections = document.querySelectorAll('.scroll-section');
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("Started");
     const sections = document.querySelectorAll('.scroll-section');
     const header = document.getElementById('header');
     const nav = document.querySelector('.navigation');
     const bannerHeight = document.querySelector('.banner').offsetHeight;
     let isNavFixed = false;
 
-    const options = {
-        threshold: 0.1
-    };
     
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, options);
+    // SPA content management
+    const links = document.querySelectorAll('.nav-link')
+    const contentDiv = document.getElementById('content')
+    console.log('The retreved consts are ', links, contentDiv);
     
-    sections.forEach(section => {
-        observer.observe(section);
-    });
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            console.log('clicked');
+            const url = this.getAttribute('href');
+            console.log('got the href', url);
+            
+            fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
+                contentDiv.innerHTML = data;
+                loadSections();
+            })
+            
+            .catch(error => {
+                console.error('Error fetching content:', error);
+                contentDiv.innerHTML = '<p>Sorry, an error occurred while loading the content.</p>';
+            })
+            console.log(contentDiv.innerHTML)
+        })
+    })
+    
+    loadSections();
 
     window.addEventListener('scroll', function() {
         if (window.scrollY >= bannerHeight && !isNavFixed) {
